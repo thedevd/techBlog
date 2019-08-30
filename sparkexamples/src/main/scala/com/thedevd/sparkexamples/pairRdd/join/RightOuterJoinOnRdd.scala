a1ddcd1e958d95be2660edf973dcae53392f8039
Devendra Vishwakarma <devendra.vishwakarma@renovite.com>
2019-08-30 17:53:26 +0530

    PairRdd- rightOuterJoin

--- sparkexamples/src/main/scala/com/thedevd/sparkexamples/pairRdd/join/RightOuterJoinOnRdd.scala
+++ sparkexamples/src/main/scala/com/thedevd/sparkexamples/pairRdd/join/RightOuterJoinOnRdd.scala
@@ -4,6 +4,16 @@ import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import org.apache.spark.sql.SparkSession
 
+/*
+ * Read the emp details from /rdd/join/emp.txt and department details from /rdd/join/dept.txt
+ * 
+ * emp.txt has the details of dept_id which is acting as foreign key for dept_id in dept.txt.
+ * 
+ * Write a spark program to show the dept details where no employee is yet working (in following format-
+ * 
+ * dept_id,dept_name
+ */
+
 object RightOuterJoinOnRdd {
 
   def main(args: Array[String]): Unit = {
@@ -36,8 +46,8 @@ object RightOuterJoinOnRdd {
       val deptWhereNoEmpWorking = emp_right_join_dept_on_deptId.filter(_._2._1 == None) // Where emp details is None
       .sortBy(_._1, ascending= true)
       
-      println("Dept where no employee is working currently")
-      for( t <- deptWhereNoEmpWorking.collect() ) println(t._2._2)
+      println("dept_id,dept_name")
+      for( t <- deptWhereNoEmpWorking.collect() ) println(t._2._2._1 + "," + t._2._2._2)
      
   }
 }
