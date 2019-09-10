60e3a758da105bbfa555538a1ad9e5961e269df7
Devendra Vishwakarma <devendra.vishwakarma@renovite.com>
2019-09-10 19:34:44 +0530

    longAccumulator example

---
+++ sparkexamples/src/main/scala/com/thedevd/sparkexamples/sharedvariables/Accumulator.scala
@@ -0,0 +1,43 @@
+package com.thedevd.sparkexamples.sharedvariables
+
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.util.AccumulatorV2
+
+/*
+ * 1. Accumulators are another type of shared variables in spark that are used to
+ *    perform write operations like counters and sum operations across workers nodes.
+ *
+ * 		One of the most common use of Accumulator is count particular events that may help in debugging process.
+ * 
+ * 2. Accumulator is creates using sparkContext.longAccumulator() 
+ * 3. Accumulator is updated using add() method
+ * 4. At drive value() is used to read the final value.
+ *
+ * For ex: we have input file /sparksql/accumulator.txt consisting blank lines which we want to count.
+ *
+ */
+object Accumulator {
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark = SparkSession.builder()
+      .appName("Accumulator")
+      .master("local[*]")
+      .getOrCreate()
+      
+    val counter = spark.sparkContext.longAccumulator("blanklines")
+    
+    val blank_lines = spark.sparkContext.textFile(getClass.getResource("/sparksql/accumulator.txt").getPath, 4)
+     .filter(_.trim().length() == 0)
+     .foreach(line => counter.add(1)) // add() is used to write into accumulator
+     
+     
+    println("no of blank lines: " + counter.value) // no of blank lines: 4
+     
+      
+  }
+
+}
