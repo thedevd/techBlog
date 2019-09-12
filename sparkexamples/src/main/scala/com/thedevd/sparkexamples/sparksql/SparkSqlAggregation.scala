006badaa9b470e612161669f62415d3e4eb5c12d
Devendra Vishwakarma <devendra.vishwakarma@renovite.com>
2019-09-12 16:08:31 +0530

    SparkSqlAggregation- finding min and max scoring subject name

--- sparkexamples/src/main/scala/com/thedevd/sparkexamples/sparksql/SparkSqlAggregation.scala
+++ sparkexamples/src/main/scala/com/thedevd/sparkexamples/sparksql/SparkSqlAggregation.scala
@@ -104,5 +104,56 @@ object SparkSqlAggregation {
     * |  8|     Juan|  Physics|    69.0|
     * +---+---------+---------+--------+*/
 
+    // #################### Lets try to solve this problem using sql style
+    println("############### Using sql text style")
+    student_report_card_df.createOrReplaceTempView("student_score_card")
+    val e1 = spark.sql("select a.id,a.name,a.subject,b.min_score from student_score_card a,"
+      + " (select id, name, min(score) min_score from student_score_card group by id, name) b"
+      + " where a.id = b.id and a.name = b.name and a.score = b.min_score")
+    e1.show()
+
+    /* +---+---------+---------+---------+
+     * | id|     name|  subject|min_score|
+     * +---+---------+---------+---------+
+     * |  1|   Joseph|  Physics|     74.0|
+     * |  2|    Jimmy|  Physics|     62.0|
+     * |  3|     Tina|Chemistry|     68.0|
+     * |  4|   Thomas|  Biology|     74.0|
+     * |  6|     Cory|    Maths|     56.0|
+     * |  7|Jackeline|  Physics|     62.0|
+     * |  8|     Juan|  Biology|     60.0|
+     * +---+---------+---------+---------+*/
+
+    val e2 = spark.sql("select a.id,a.name,a.subject,b.max_score from student_score_card a,"
+      + " (select id, name, max(score) max_score from student_score_card group by id, name) b"
+      + " where a.id = b.id and a.name = b.name and a.score = b.max_score")
+
+    e2.show()
+
+    /* +---+---------+---------+---------+
+      * | id|     name|  subject|max_score|
+      * +---+---------+---------+---------+
+      * |  1|   Joseph|Chemistry|     91.0|
+      * |  2|    Jimmy|Chemistry|     97.0|
+      * |  3|     Tina|  Biology|     87.0|
+      * |  4|   Thomas|  Physics|     93.0|
+      * |  6|     Cory|Chemistry|     71.0|
+      * |  7|Jackeline|    Maths|     86.0|
+      * |  8|     Juan|  Physics|     69.0|
+      * +---+---------+---------+---------+*/
+
+    e1.join(e2, Seq("id", "name"), "inner").show()
+    /* +---+---------+---------+---------+---------+---------+
+     * | id|     name|  subject|min_score|  subject|max_score|
+     * +---+---------+---------+---------+---------+---------+
+     * |  1|   Joseph|  Physics|     74.0|Chemistry|     91.0|
+     * |  2|    Jimmy|  Physics|     62.0|Chemistry|     97.0|
+     * |  3|     Tina|Chemistry|     68.0|  Biology|     87.0|
+     * |  4|   Thomas|  Biology|     74.0|  Physics|     93.0|
+     * |  6|     Cory|    Maths|     56.0|Chemistry|     71.0|
+     * |  7|Jackeline|  Physics|     62.0|    Maths|     86.0|
+     * |  8|     Juan|  Biology|     60.0|  Physics|     69.0|
+     * +---+---------+---------+---------+---------+---------+
+     * */
   }
 }
