eb62b65e7dd7d7c46d2e924e5cb6b0cbf3f77ed5
Devendra Vishwakarma <devendra.vishwakarma@renovite.com>
2019-10-22 12:28:52 +0530

    for/yield vs map

---
+++ scalaexamples/src/main/scala/com/thedevd/scalaexamples/basics/ScalaForComprehensionVsMap.scala
@@ -0,0 +1,66 @@
+package com.thedevd.scalaexamples.basics
+
+/*
+ * Rather than using for/yield comprehension, we can call the map function on the collection
+ * to transform to new collection. Map function takes a function as input and that function
+ * should take atleast one parameter.
+ *
+ * But once you add a guard (if statments), a for/yield loop is no longer directly equivalent to
+ * just a map method call.
+ *
+ * In this demo we will see this.
+ */
+object ScalaForComprehensionVsMap {
+
+  def main(args: Array[String]): Unit = {
+
+    example1_toUpperCase()
+    example2_useOfGuard()
+
+  }
+
+  def example1_toUpperCase() = {
+    val namelist = List("dev", "ravi", "atul", "ankit")
+
+    val upperNameList_for = for (name <- namelist) yield name.toUpperCase()
+    println(upperNameList_for) // --> List(DEV, RAVI, ATUL, ANKIT)
+
+    val upperNameList_map = namelist.map(name => name.toUpperCase())
+    println(upperNameList_map) // --> List(DEV, RAVI, ATUL, ANKIT)
+
+    /*
+     * Passing function as parameter to map
+     */
+    val toUpper = (name: String) => name.toUpperCase()
+    println(namelist.map(toUpper)) // --> List(DEV, RAVI, ATUL, ANKIT)
+    
+  }
+  
+  
+  def example2_useOfGuard() = {
+    /*
+     * As we saw in example1, that for simple usecase for/yield is same to map.
+     * But once you add a guard, a for/yield loop is no longer directly equivalent to just a map method call
+     */
+    
+    val namelist = List("dev", "ravi", "atul", "ankit")
+    val upperNameList_for = for(name <- namelist if(name.length() > 3)) yield name.toUpperCase()
+    println(upperNameList_for) // --> List(RAVI, ATUL, ANKIT)
+    
+    // let see the outcome of applying similar logic here
+    val toUpper = (name: String) => if(name.length() > 3) name.toUpperCase()
+    val upperNameList_map = namelist.map(toUpper)
+    println(upperNameList_map) // --> List((), RAVI, ATUL, ANKIT)
+    /*
+     * So you can see, if guard is there in form of if statement, then map produces different result
+     * (one of the output is empty List((), RAVI, ATUL, ANKIT)
+     * 
+     * So in such case you can consider guard as filter(), and to achieve the correct result using map
+     * you need to filter the elements first before applying map. Lets see how
+     */
+    
+    val correctNameList_map = namelist.filter(_.length() > 3).map(toUpper)
+    println(correctNameList_map) // --> List(RAVI, ATUL, ANKIT)
+    
+  }
+}
