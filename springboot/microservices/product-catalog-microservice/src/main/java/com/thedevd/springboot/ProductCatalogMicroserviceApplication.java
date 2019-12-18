8fb638dc89c26dfcfedd66d998f16a6d28ba8944
Devendra Vishwakarma <devendra.vishwakarma@renovite.com>
2019-12-18 20:25:28 +0530

    configuration to register with Eureka-Server

--- springboot/microservices/product-catalog-microservice/src/main/java/com/thedevd/springboot/ProductCatalogMicroserviceApplication.java
+++ springboot/microservices/product-catalog-microservice/src/main/java/com/thedevd/springboot/ProductCatalogMicroserviceApplication.java
@@ -1,11 +1,21 @@
 package com.thedevd.springboot;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient // Enable DiscoveryClient implementation of Eureka-Client
 public class ProductCatalogMicroserviceApplication {
 
 	public static void main(String[] args) {
@@ -13,3 +23,17 @@ public class ProductCatalogMicroserviceApplication {
 	}
 
 }
+
+@RestController
+class ServiceInstanceRestController {
+
+  @Autowired
+  private DiscoveryClient discoveryClient;
+
+  @GetMapping("/service-instances/{applicationName}")
+  public List<ServiceInstance> serviceInstancesByApplicationName(
+      @PathVariable String applicationName) {
+    return this.discoveryClient.getInstances(applicationName);
+  }
+}
+
