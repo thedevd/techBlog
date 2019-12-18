e96f501c3396c4bc80294015930f69efa7fc6225
Devendra Vishwakarma <devendra.vishwakarma@renovite.com>
2019-12-18 15:42:41 +0530

    Created Netflix-Eureka naming server for Service Registration and discovery

---
+++ springboot/microservices/netflix-eureka-naming-server/src/main/java/com/thedevd/springboot/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,15 @@
+package com.thedevd.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // this annotation is used to enable Eureka Server and configure it
+public class NetflixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+	}
+
+}
