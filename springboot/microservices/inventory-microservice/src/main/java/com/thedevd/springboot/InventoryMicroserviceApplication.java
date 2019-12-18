f7b243f62a0121e7fa009ed713282b2e6514bcc9
Devendra Vishwakarma <devendra.vishwakarma@renovite.com>
2019-12-18 20:24:21 +0530

    configuration to register with Eureka-Server

--- springboot/microservices/inventory-microservice/src/main/java/com/thedevd/springboot/InventoryMicroserviceApplication.java
+++ springboot/microservices/inventory-microservice/src/main/java/com/thedevd/springboot/InventoryMicroserviceApplication.java
@@ -1,13 +1,37 @@
 package com.thedevd.springboot;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient // Enable the client implementation for Eureka-Client
 public class InventoryMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryMicroserviceApplication.class, args);
 	}
 
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+  @Autowired
+  private DiscoveryClient discoveryClient;
+
+  @GetMapping("/service-instances/{applicationName}")
+  public List<ServiceInstance> serviceInstancesByApplicationName(
+      @PathVariable String applicationName) {
+    return this.discoveryClient.getInstances(applicationName);
+  }
 }
