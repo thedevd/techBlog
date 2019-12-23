247da685a5bf5f811b814cda31cf530451dd8905
Devendra Vishwakarma <devendra.vishwakarma@renovite.com>
2019-12-23 19:06:12 +0530

    Setup of netflix-zuul api gateway server

---
+++ springboot/microservices/netflix-zuul-api-gateway-server/src/main/java/com/thedevd/springboot/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,17 @@
+package com.thedevd.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy // To make it a Zuul proxy server, added this annotation
+@EnableDiscoveryClient
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+
+}
