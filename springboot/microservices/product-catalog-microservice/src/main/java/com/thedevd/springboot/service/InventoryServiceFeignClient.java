5d46d04725b4030eec57ac3e2286682021715368
Devendra Vishwakarma <devendra.vishwakarma@renovite.com>
2019-12-24 19:04:27 +0530

    configured InventoryServiceFeignClient to talk Zuul not the end microservice

--- springboot/microservices/product-catalog-microservice/src/main/java/com/thedevd/springboot/service/InventoryServiceFeignClient.java
+++ springboot/microservices/product-catalog-microservice/src/main/java/com/thedevd/springboot/service/InventoryServiceFeignClient.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // @FeignClient(name = "inventory-service", url = "localhost:8082")
-@FeignClient(name = "inventory-service")
-@RibbonClient(name = "inventory-service")
+// @FeignClient(name = "inventory-service")
+// zuul: tell the Feign client to talk to Zuul API gateway instead of  end microservice.
+@FeignClient(name = "netflix-zuul-api-gateway-server")
+// @RibbonClient(name = "inventory-service")
 public interface InventoryServiceFeignClient {
 
-	@GetMapping("/api/inventory/{productCode}")
+	// zuul: update all the mappings to start with application-name of inventory-microservice application.
+	//@GetMapping("/api/inventory/{productCode}")
+	@GetMapping("/inventory-service/api/inventory/{productCode}")
 	public InventoryItemResponse getInventoryByProductCode(@PathVariable("productCode") String productCode);
 }
